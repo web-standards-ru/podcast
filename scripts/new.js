8ac16fac5cfb9d40ec6b66ac9ae7527ba1f61ae1
mefody <mefody@yandex-team.ru>
2024-04-30 18:22:25 +0300

    Добавляет больше автоматизации в скрипт new

--- scripts/new.js
+++ scripts/new.js
@@ -1,27 +1,34 @@
 import fs from "node:fs";
 
-const episode = Number(process.env.npm_config_episode);
+// Добавляет эпизод в README
+let readmeContent = fs.readFileSync("README.md").toString();
+const prevEpisode = Number(
+	readmeContent.match(/\[(\d+)\]: src\/episodes\/(\d+)\/index.md/)[1]
+);
 
-if (!episode) {
-	console.error(
-		"Нужен номер эпизода, например: npm run new --episode=417"
-	);
+if (!prevEpisode) {
+	console.error("Не получилось достать номер прошлого эпизода из README.md.");
 	process.exit(1);
 }
 
-// Добавляет эпизод в README
-function episodeLinkString(episode) {
-	return `[${episode}]: src/episodes/${episode}/index.md`;
+const episode = prevEpisode + 1;
+
+if (!readmeContent.includes(`| ${episode}     |`)) {
+	readmeContent = readmeContent.replace(
+		"<details open>",
+		`${generateNewTable(episode)}
+
+<details>`
+	);
 }
 
-const readmeContent = fs
-	.readFileSync("README.md")
-	.toString()
+readmeContent = readmeContent
 	.replace(
 		episodeLinkString(episode - 1),
 		`${episodeLinkString(episode)}\n${episodeLinkString(episode - 1)}`
 	)
 	.replace(`| ${episode}     |`, `| [${episode}][] |`);
+
 fs.writeFileSync("README.md", readmeContent);
 
 // Создаёт файлы для нового эпизода
@@ -45,3 +52,30 @@ const episodeYmlContent = fs
 	.toString()
 	.replace("date: 3000-01-01T09:00", `date: ${mondayString}`);
 fs.writeFileSync(`src/episodes/${episode}/index.yml`, episodeYmlContent);
+
+console.info(`Пустышки для эпизода ${episode} сгенерированы. Теперь заполни их:
+- src/episodes/${episode}/index.yml
+- src/episodes/${episode}/index.md
+`);
+
+function episodeLinkString(episode) {
+	return `[${episode}]: src/episodes/${episode}/index.md`;
+}
+
+function generateNewTable(episode) {
+	const startNumber = Math.floor(episode / 100) * 100 + 100;
+	let table = "";
+	for (let i = 0; i < 10; i++) {
+		for (let j = 0; j < 10; j++) {
+			table += `| ${startNumber - i * 10 - j}     `;
+		}
+		table += "|\n";
+	}
+	return `<details open>
+	<summary>№${startNumber}–${startNumber - 99}</summary>
+
+| №       |         |         |         |         |         |         |         |         |         |
+| ------- | ------- | ------- | ------- | ------- | ------- | ------- | ------- | ------- | ------- |
+${table}
+</details>`;
+}
