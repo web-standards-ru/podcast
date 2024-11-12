fbcd53e973f21e7a383d343d34bece9f021eab59
monochromer <monochromer@users.noreply.github.com>
2024-11-12 19:27:19 +0500

    Добавляет правила для преобразования списков в параграфы в markdown (#24)
    * Добавляет правила для преобразования списков в параграфы в markdown
    
    * Убирает список из таймкодов
    
    * Убирает слэш
    
    ---------
    
    Co-authored-by: Vadim Makeev <hi@pepelsbey.dev>

--- scripts/strip-tags.js
+++ scripts/strip-tags.js
@@ -14,4 +14,49 @@ function stripHeadings(md) {
 	});
 }
 
-export { stripHeadings };
+function stripLists(md) {
+	md.core.ruler.push('strip_lists', (state) => {
+		let newTokens = [];
+		let isListItemProcessing = false;
+
+		state.tokens.forEach((token) => {
+			if (token.type === 'bullet_list_open') {
+				const listOpenToken = new state.Token('html_paragraph', 'p', 1);
+				newTokens.push(listOpenToken);
+				return;
+			}
+
+			if (token.type === 'bullet_list_close') {
+				const listCloseToken = new state.Token('html_paragraph', 'p', -1);
+				newTokens.push(listCloseToken);
+				return;
+			}
+
+			if (token.type === 'list_item_open') {
+				isListItemProcessing = true;
+				return;
+			}
+
+			if (token.type === 'list_item_close') {
+				isListItemProcessing = false;
+				return;
+			}
+
+			if (token.type === 'inline' && isListItemProcessing) {
+				const bulletToken = new state.Token('html_inline', '', 0);
+				bulletToken.content = '• ';
+				token.children.unshift(bulletToken);
+
+				const breakToken = new state.Token('html_inline', '', 0);
+				breakToken.content = '<br>';
+				token.children.push(breakToken);
+			}
+
+			newTokens.push(token);
+		});
+
+		state.tokens = newTokens;
+	});
+}
+
+export { stripHeadings, stripLists };
